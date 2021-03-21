964ac69ac44b97b8a57da0055fb9c0fced9d3170
gorhawk <gorhawk@users.noreply.github.com>
2021-03-21 23:25:49 +0100

    added new music player WIP

---
+++ src/components/MusicPlayer.tsx
@@ -0,0 +1,151 @@
+import React, { useRef, useState } from "react";
+import ReactPlayer from "react-player";
+import { createUseStyles, Styles } from "react-jss";
+import Play from "./icons/Play";
+import Pause from "./icons/Pause";
+
+const playerElementMargin = [["auto", 5]];
+
+const useStyles = createUseStyles({
+  player: {
+    display: "flex",
+    width: "100%",
+    height: 28,
+    border: "1px solid red",
+  },
+  playButton: {
+    flex: [[0, 0, "auto"]],
+    width: 12,
+    border: "none",
+    margin: playerElementMargin,
+    padding: 0,
+    background: "transparent",
+  },
+  timer: {
+    flex: [[0, 0, "auto"]],
+    fontWeight: 700,
+    fontSize: 11,
+    margin: playerElementMargin,
+  },
+  rangeWrapper: {
+    left: -8,
+    right: -8,
+    position: "absolute",
+    height: 10,
+    opacity: 0,
+  },
+  rangeBar: {
+    position: "absolute",
+    width: "100%",
+    height: 10,
+    cursor: "pointer",
+  },
+  progressBar: {
+    position: "relative",
+    flex: [[1, 1, "auto"]],
+    margin: playerElementMargin,
+    height: 8,
+    borderRadius: 3,
+    background: "grey",
+  },
+  progressBand: {
+    borderRadius: 3,
+    position: "absolute",
+    height: 8,
+    background: "white",
+  },
+  volumeBar: {
+    position: "relative",
+    flex: [[0, 1, "80px"]],
+    margin: playerElementMargin,
+    height: 8,
+    borderRadius: 3,
+    background: "grey",
+  },
+  volumeBand: {
+    borderRadius: 3,
+    position: "absolute",
+    height: 8,
+    background: "white",
+  },
+});
+
+const MusicPlayer = ({ urls }) => {
+  const classes = useStyles();
+  const player = useRef(null);
+  const [played, setPlayed] = useState(0);
+  const [volume, setVolume] = useState(0.5);
+  const [isPlaying, setPlaying] = useState(false);
+  const [isSeeking, setSeeking] = useState(false);
+
+  return (
+    <div className={classes.player}>
+      <ReactPlayer
+        ref={player}
+        playing={isPlaying}
+        url={urls}
+        onProgress={(state) => {
+          if (isSeeking) {
+            return;
+          }
+          setPlayed(state.played);
+        }}
+        volume={volume}
+        width={0}
+        height={0}
+      />
+      <button
+        className={classes.playButton}
+        title="Play/Pause"
+        onClick={() => setPlaying(!isPlaying)}
+      >
+        {isPlaying ? <Pause fill="white" /> : <Play fill="white" />}
+      </button>
+      <div className={classes.timer}>00:00</div>
+      <div className={classes.progressBar}>
+        <div
+          className={classes.progressBand}
+          style={{ width: `${played * 100}%` }}
+        />
+        <div className={classes.rangeWrapper}>
+          <input
+            className={classes.rangeBar}
+            type="range"
+            min={0}
+            max={0.999999}
+            step="any"
+            value={played}
+            onMouseDown={() => setSeeking(true)}
+            onChange={(event) => setPlayed(parseFloat(event.target.value))}
+            onMouseUp={(event) => {
+              player.current?.seekTo(
+                parseFloat((event.target as HTMLInputElement).value)
+              );
+              setSeeking(false);
+            }}
+          />
+        </div>
+      </div>
+      <div className={classes.timer}>00:00</div>
+      <div className={classes.volumeBar}>
+        <div className={classes.rangeWrapper}>
+          <div
+            className={classes.volumeBand}
+            style={{ width: `${volume * 100}%` }}
+          />
+          <input
+            className={classes.rangeBar}
+            type="range"
+            min={0}
+            max={1}
+            step={0.1}
+            value={volume}
+            onChange={(event) => setVolume(parseFloat(event.target.value))}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MusicPlayer;
--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -2,11 +2,14 @@ import { Link } from "gatsby";
 import React, { FC } from "react";
 import FilterBar from "../components/FilterBar";
 import Layout from "../components/Layout";
+import MusicPlayer from "../components/MusicPlayer";
 
 const Music: FC = () => (
   <Layout>
     <FilterBar />
+    <MusicPlayer urls={["https://previews.customer.envatousercontent.com/files/316563288/preview.mp3"]}/>
   </Layout>
 );
+
 
 export default Music;
